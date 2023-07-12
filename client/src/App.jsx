02d79bbbaf858848e2b271814d94cc6fb31d0e79
M45Hasan <mmhasan045@gmail.com>
2023-07-12 17:23:07 +0600

    NavbarFooter design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
 import Regi from "./pages/Regi";
+import Term from "./pages/Term";
 
 function App() {
   return <>
-  <Regi/>
+ <Navbar/>
   </>
 }
 
